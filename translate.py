33b9cead7f483d65ac17d06faac1636df378656c
2439905184 <29478722+2439905184@users.noreply.github.com>
2020-06-20 21:52:21 +0800

    a

---
+++ translate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+tta=file.readline()
+taa=file.readline()
+print(taa)
+
+for index in range(100):
+    current_char=tta[index]
+    print(current_char)
+
+file.close()
+#config.read("201.iet","utf-8")
