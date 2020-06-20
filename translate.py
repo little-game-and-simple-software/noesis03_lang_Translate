d1bd9590967c4f4840fd09204f316c3278ca3368
2439905184 <29478722+2439905184@users.noreply.github.com>
2020-06-20 22:38:25 +0800

    应该先想好再写代码的

--- translate.py
+++ translate.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+file=open("201.iet",'r',-1,"utf-8")
+lines=file.readlines()
+index=1
 
-tta=file.readline()
-taa=file.readline()
-print(taa)
-
-for index in range(100):
-    current_char=tta[index]
-    print(current_char)
-
+for line in lines:
+    #print(index)
+   # tmp_char=line[index]
+    if "t" in line:
+        print("当前行")
+        print(line)
+        t_pos=line.find("t")
+        print("t在当前行的位置"+str(t_pos))
+        print("t的下一个字符"+str(line[t_pos+1]))
+        s=input()
+        if s=="y":
+            pass
+        else:
+            break
+    index+=1
+           
 file.close()
 #config.read("201.iet","utf-8")
